7adb89dcd75f20688b741cb8233e9354bffb015d
道厘花清 <taketori01plus64@ezweb.ne.jp>
2017-10-02 18:53:57 +0900

    Lesson5 PinBallをつくろう
    Lesson5の提出課題です。発展課題は断念しました。

---
+++ Assets/ScoreController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour {
+	//初期化
+	private int score = 0;
+	//スコアを表示するテキスト
+	private GameObject scoreText;
+
+	// Use this for initialization
+	void Start () {
+		//ScoreTextオブジェクトを取得？
+		this.scoreText = GameObject.Find("ScoreText");
+		//初期スコアを表示
+		this.scoreText.GetComponent<Text> ().text = "0";
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//スコアを表示
+		this.scoreText.GetComponent<Text> ().text = score.ToString();
+
+	}
+	void OnCollisionEnter(Collision other){
+
+		// タグによって得点を変える
+		if(other.gameObject.tag == "SmallStarTag") {
+			this.score += 10;
+		}else if(other.gameObject.tag == "LargeStarTag") {
+			this.score += 20;
+		}else if(other.gameObject.tag == "SmallCloudTag") {
+			this.score += 30;
+		}else if(other.gameObject.tag == "LargeCloudTag") {
+			this.score += 50;
+		}
+
+		//コンソールに表示
+		Debug.Log(score);
+	}
+
+}
